1d74e4c8469765136b72e270054f5c1c55295e7f
syaning <syaningv@gmail.com>
2016-08-25 14:50:36 +0800

    export RequestWrapper

--- README.md
+++ README.md
@@ -8,6 +8,25 @@ The most simple but handy HTTP request library for Go.
 $ go get github.com/syaning/ok
 ```
 
+## Getting Started
+
+### HTTP Get
+
+```go
+str, err := ok.
+    Get("http://httpbin.org/get").
+    OK().
+    ToString()
+fmt.Println(str, err)
+```
+
+### Download
+
+```go
+size, err := ok.Download("http://httpbin.org/image/png", "img.png")
+fmt.Println(size, err)
+```
+
 ## License
 
 [MIT](./LICENSE)
--- ok.go
+++ ok.go
@@ -9,59 +9,59 @@ import (
 	"strings"
 )
 
-type request struct {
+type RequestWrapper struct {
 	client *http.Client
 	req    *http.Request
 	res    *http.Response
 	err    error
 }
 
-func NewRequest(method, urlStr string) *request {
+func NewRequest(method, urlStr string) *RequestWrapper {
 	req, err := http.NewRequest(method, urlStr, nil)
 	if err != nil {
 		return nil
 	}
-	return &request{req: req}
+	return &RequestWrapper{req: req}
 }
 
-func Request() *request {
+func Request() *RequestWrapper {
 	return NewRequest("", "")
 }
 
-func Get(urlStr string) *request {
+func Get(urlStr string) *RequestWrapper {
 	return NewRequest("GET", urlStr)
 }
 
-func Post(urlStr string) *request {
+func Post(urlStr string) *RequestWrapper {
 	return NewRequest("POST", urlStr)
 }
 
-func Put(urlStr string) *request {
+func Put(urlStr string) *RequestWrapper {
 	return NewRequest("PUT", urlStr)
 }
 
-func Delete(urlStr string) *request {
+func Delete(urlStr string) *RequestWrapper {
 	return NewRequest("DELETE", urlStr)
 }
 
-func (r *request) Client() *http.Client {
+func (r *RequestWrapper) Client() *http.Client {
 	return r.client
 }
 
-func (r *request) Request() *http.Request {
+func (r *RequestWrapper) Request() *http.Request {
 	return r.req
 }
 
-func (r *request) Response() (*http.Response, error) {
+func (r *RequestWrapper) Response() (*http.Response, error) {
 	return r.res, r.err
 }
 
-func (r *request) Method(method string) *request {
+func (r *RequestWrapper) Method(method string) *RequestWrapper {
 	r.req.Method = method
 	return r
 }
 
-func (r *request) Url(urlStr string) *request {
+func (r *RequestWrapper) Url(urlStr string) *RequestWrapper {
 	u, err := url.Parse(urlStr)
 	if err != nil {
 		return r
@@ -70,32 +70,32 @@ func (r *request) Url(urlStr string) *request {
 	return r
 }
 
-func (r *request) Query(query string) *request {
+func (r *RequestWrapper) Query(query string) *RequestWrapper {
 	r.req.URL.RawQuery = query
 	return r
 }
 
-func (r *request) Param(key, value string) *request {
+func (r *RequestWrapper) Param(key, value string) *RequestWrapper {
 	query := r.req.URL.Query()
 	query.Add(key, value)
 	return r.Query(query.Encode())
 }
 
-func (r *request) Set(key, value string) *request {
+func (r *RequestWrapper) Set(key, value string) *RequestWrapper {
 	r.req.Header.Set(key, value)
 	return r
 }
 
-func (r *request) Header(key, value string) *request {
+func (r *RequestWrapper) Header(key, value string) *RequestWrapper {
 	return r.Set(key, value)
 }
 
-func (r *request) BasicAuth(username, password string) *request {
+func (r *RequestWrapper) BasicAuth(username, password string) *RequestWrapper {
 	r.req.SetBasicAuth(username, password)
 	return r
 }
 
-func (r *request) Form(data string) *request {
+func (r *RequestWrapper) Form(data string) *RequestWrapper {
 	reader := strings.NewReader(data)
 	r.req.Body = ioutil.NopCloser(reader)
 	r.req.ContentLength = int64(reader.Len())
@@ -103,7 +103,7 @@ func (r *request) Form(data string) *request {
 	return r
 }
 
-func (r *request) Json(data string) *request {
+func (r *RequestWrapper) Json(data string) *RequestWrapper {
 	reader := strings.NewReader(data)
 	r.req.Body = ioutil.NopCloser(reader)
 	r.req.ContentLength = int64(reader.Len())
@@ -111,11 +111,11 @@ func (r *request) Json(data string) *request {
 	return r
 }
 
-func (r *request) JSON(data string) *request {
+func (r *RequestWrapper) JSON(data string) *RequestWrapper {
 	return r.Json(data)
 }
 
-func (r *request) Proxy(proxy string) *request {
+func (r *RequestWrapper) Proxy(proxy string) *RequestWrapper {
 	r.lazyClient()
 	r.client.Transport = &http.Transport{
 		Proxy: func(_ *http.Request) (*url.URL, error) {
@@ -125,26 +125,26 @@ func (r *request) Proxy(proxy string) *request {
 	return r
 }
 
-func (r *request) ProxyFn(proxyFn func(*http.Request) (*url.URL, error)) *request {
+func (r *RequestWrapper) ProxyFn(proxyFn func(*http.Request) (*url.URL, error)) *RequestWrapper {
 	r.lazyClient()
 	r.client.Transport = &http.Transport{Proxy: proxyFn}
 	return r
 }
 
-func (r *request) lazyClient() {
+func (r *RequestWrapper) lazyClient() {
 	if r.client == nil {
 		r.client = &http.Client{}
 	}
 }
 
-func (r *request) Use(client *http.Client) *request {
+func (r *RequestWrapper) Use(client *http.Client) *RequestWrapper {
 	if client != nil {
 		r.client = client
 	}
 	return r
 }
 
-func (r *request) OK() *request {
+func (r *RequestWrapper) OK() *RequestWrapper {
 	client := http.DefaultClient
 	if r.client != nil {
 		client = r.client
@@ -153,7 +153,7 @@ func (r *request) OK() *request {
 	return r
 }
 
-func (r *request) ToBytes() ([]byte, error) {
+func (r *RequestWrapper) ToBytes() ([]byte, error) {
 	res, err := r.Response()
 	if err != nil {
 		return nil, err
@@ -162,7 +162,7 @@ func (r *request) ToBytes() ([]byte, error) {
 	return ioutil.ReadAll(res.Body)
 }
 
-func (r *request) ToString() (string, error) {
+func (r *RequestWrapper) ToString() (string, error) {
 	data, err := r.ToBytes()
 	if err != nil {
 		return "", err
@@ -170,7 +170,7 @@ func (r *request) ToString() (string, error) {
 	return string(data), nil
 }
 
-func (r *request) Pipe(w io.Writer) (written int64, err error) {
+func (r *RequestWrapper) Pipe(w io.Writer) (written int64, err error) {
 	res, err := r.Response()
 	if err != nil {
 		return 0, err
@@ -180,7 +180,7 @@ func (r *request) Pipe(w io.Writer) (written int64, err error) {
 	return
 }
 
-func (r *request) ToFile(filename string) (size int64, err error) {
+func (r *RequestWrapper) ToFile(filename string) (size int64, err error) {
 	file, err := os.Create(filename)
 	if err != nil {
 		return 0, err
